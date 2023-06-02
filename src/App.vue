56e2becdc098c590c6a57e448224d4b729bf5cae
Tien Pham Nguyen <tien.phamnguyen@powergatesoftware.com>
2023-06-02 16:13:43 +0700

    Init test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="PIN Input" />
 </template>
 
 <script>
@@ -9,8 +8,8 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,104 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
+  <div>Set number of PIN: <input @input="setLengthOfPIN" /></div>
+  <br />
+  <br />
+  <div className="input-wrapper">
+    <v-otp-input
+      ref="otpInput"
+      v-model:value="bindValue"
+      input-classes="otp-input"
+      separator="-"
+      :num-inputs="lengthOfPIN"
+      :should-auto-focus="true"
+      input-type="password"
+      :conditionalClass="['one', 'two', 'three', 'four']"
+      @on-change="handleOnChange"
+      @on-complete="handleOnComplete"
+    />
+  </div>
+
+  <ul :style="gridStyle" class="card-list">
+    <li v-for="(card, index) in cards" class="card-item" :key="card" @click="fillInput(index + 1)">
+      {{ index + 1 }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import { ref, unref } from 'vue'
+import VOtpInput from 'vue3-otp-input'
+
+const otpInput = ref(null)
+const bindValue = ref('')
+const lengthOfPIN = ref(5)
+
+const handleOnComplete = () => {
+  console.log('OTP v-model:value: ', unref(bindValue))
+}
+
+const handleOnChange = (value) => {
+  if (unref(bindValue).length <= lengthOfPIN.value) {
+    bindValue.value += value.toString()
+  }
+}
+
+const clearInput = () => {
+  if (unref(otpInput)) {
+    unref(otpInput).clearInput()
+  }
+}
+
+const setLengthOfPIN = (event) => {
+  lengthOfPIN.value = Number.parseInt(event.target.value) || 5
+}
+
+const fillInput = (value) => {
+  if (unref(bindValue).length <= lengthOfPIN.value) {
+    bindValue.value += value.toString()
+  }
+}
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  components: {
+    VOtpInput,
+  },
+  data() {
+    return {
+      cards: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      numberOfColumns: 3,
+    }
+  },
+  computed: {
+    gridStyle() {
+      return {
+        gridTemplateColumns: `repeat(${this.numberOfColumns}, minmax(100px, 1fr))`,
+      }
+    },
+  },
+  methods: {
+    addCard() {
+      this.cards.push('new-card')
+    },
+    handleOnChange,
+    handleOnComplete,
+    clearInput,
+    fillInput,
+    setLengthOfPIN,
+  },
+  setup() {
+    return {
+      otpInput,
+      bindValue,
+      lengthOfPIN,
+    }
+  },
 }
 </script>
 
@@ -54,5 +117,42 @@ li {
 }
 a {
   color: #42b983;
+}
+.card-list {
+  display: grid;
+  grid-gap: 1em;
+}
+
+.card-item {
+  background-color: dodgerblue;
+  padding: 2em;
+}
+.input-wrapper {
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.otp-input {
+  width: 40px;
+  height: 40px;
+  padding: 20px;
+  font-size: 30px;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  text-align: center;
+}
+/* Background colour of an input field with value */
+.otp-input.is-complete {
+  background-color: #e4e4e4;
+}
+.otp-input::-webkit-inner-spin-button,
+.otp-input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+input::placeholder {
+  font-size: 30px;
+  text-align: center;
+  font-weight: 600;
 }
 </style>
